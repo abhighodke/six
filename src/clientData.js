578bb3d8aa8d035b05362873be8be95b09a5c4b1
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-22 21:26:15 +0530

    Upload

---
+++ src/clientData.js
@@ -0,0 +1,37 @@
+// src/clientData.js
+const clientData = {
+    headerInitials: "MD", // Header Initials
+    heroTagline: "Mindy Dilley Realty", // Hero Tagline
+    agentName: "Mindy Dilley",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.", // AboutMe Short Description
+    aboutMeDetailed: `We are longtime Las Vegas residents with 3 wonderful children and 2 beautiful granddaughters.
+John, a Marine Corps veteran with 9+ years corporate management experience, combines his vast
+business knowledge with Mindy, who has 10+ years in real estate. We incorporate our passion for
+real estate with our love for Las Vegas to create a one-of-a-kind personal real estate experience.
+Integrated with world-class customer service by listening to our customers, understanding their
+needs, and keeping an open line of communication. These, combined with building a relationship
+based on trust, are how we make your Las Vegas real estate dreams come true.`, // AboutMe Detailed Description
+    testimonials: [
+        { quote: "Working with Mindy was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
+        { quote: "Mindy Dilley Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
+        { quote: "The dedication and expertise of Mindy were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
+        { quote: "Mindy and her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
+        { quote: "If you’re looking for a real estate agent who goes above and beyond, Mindy is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
+    ], // Testimonials
+    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
+    email: "dilleydilleylv@gmail.com", // Email
+    address: "10845 Griffith Peak Dr. Ste. 2, Las Vegas , NV, 89135", // Address
+    phone: "702-755-0477", // Phone Number
+    footerDescription: "Mindy Dilley Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+    socialMediaLinks: {
+        facebook: "",
+        Instagram: "",
+        linkedin: "",
+    }, // Social Media Links
+    contactDetails: {
+        email: "dilleydilleylv@gmail.com",
+        phone: "702-755-0477",
+    }, // Contact Details
+};
+
+export default clientData;
