9c153b6099faabbe0e62a8c8c03d17de06a7674a
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:34:59 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,37 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "MD", // Header Initials
-    heroTagline: "Mindy Dilley Realty", // Hero Tagline
-    agentName: "Mindy Dilley",
-    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.", // AboutMe Short Description
-    aboutMeDetailed: `We are longtime Las Vegas residents with 3 wonderful children and 2 beautiful granddaughters.
-John, a Marine Corps veteran with 9+ years corporate management experience, combines his vast
-business knowledge with Mindy, who has 10+ years in real estate. We incorporate our passion for
-real estate with our love for Las Vegas to create a one-of-a-kind personal real estate experience.
-Integrated with world-class customer service by listening to our customers, understanding their
-needs, and keeping an open line of communication. These, combined with building a relationship
-based on trust, are how we make your Las Vegas real estate dreams come true.`, // AboutMe Detailed Description
-    testimonials: [
-        { quote: "Working with Mindy was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-        { quote: "Mindy Dilley Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-        { quote: "The dedication and expertise of Mindy were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-        { quote: "Mindy and her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-        { quote: "If you’re looking for a real estate agent who goes above and beyond, Mindy is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "dilleydilleylv@gmail.com", // Email
-    address: "10845 Griffith Peak Dr. Ste. 2, Las Vegas , NV, 89135", // Address
-    phone: "702-755-0477", // Phone Number
-    footerDescription: "Mindy Dilley Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+    headerInitials: "MHL",
+    heroTagline: "Mary Haines-Lovin Realty",
+    agentName: "Mary Haines-Lovin",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
+
+With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
+
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with Mary Haines-Lovin was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Mary Haines-Lovin provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Mary Haines-Lovin were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Mary Haines-Lovin and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Mary Haines-Lovin is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "haines.mary@yahoo.com",
+    address: "Robertsdale, Alabama",
+    phone: "251-233-9221",
+    footerDescription: `Mary Haines-Lovin Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
+        facebook: "https://www.facebook.com/haines.mary/",
         Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
-        email: "dilleydilleylv@gmail.com",
-        phone: "702-755-0477",
-    }, // Contact Details
+        email: "haines.mary@yahoo.com",
+        phone: "251-233-9221",
+    },
+    website: "",
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/434131902_26223622967237104_7193531624326877627_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=e8qVgul68hQQ7kNvgHCBwIm&_nc_ht=scontent-sjc3-1.xx&_nc_gid=APuX9q6HxL9hRDvgXeTLeq1&oh=00_AYCKzcjPddgLiOVjPVSHVOZx-KybbOgWns-yA7QuDL2yOQ&oe=66D7DC8D" // Agent image URL
 };
 
 export default clientData;
