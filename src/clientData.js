ea5a0766838fafac24f32c3f6711e0e585431240
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:37:29 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "MHL",
-    heroTagline: "Mary Haines-Lovin Realty",
-    agentName: "Mary Haines-Lovin",
+    headerInitials: "MC",
+    heroTagline: "Michelle Creamer Realty",
+    agentName: "Michelle Creamer",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Mary Haines-Lovin was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Mary Haines-Lovin provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Mary Haines-Lovin were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Mary Haines-Lovin and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Mary Haines-Lovin is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Michelle Creamer was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Michelle Creamer provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Michelle Creamer were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Michelle Creamer and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Michelle Creamer is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "haines.mary@yahoo.com",
-    address: "Robertsdale, Alabama",
-    phone: "251-233-9221",
-    footerDescription: `Mary Haines-Lovin Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "mcreamer@arcrealtyco.com",
+    address: "3215 Endeavor Lane Suite 113 \nBirmingham, AL 35242",
+    phone: "(205) 999-8164",
+    footerDescription: `Michelle Creamer Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/haines.mary/",
+        facebook: "https://www.facebook.com/michelle.creamer.96/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "haines.mary@yahoo.com",
-        phone: "251-233-9221",
+        email: "mcreamer@arcrealtyco.com",
+        phone: "(205) 999-8164",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/434131902_26223622967237104_7193531624326877627_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=e8qVgul68hQQ7kNvgHCBwIm&_nc_ht=scontent-sjc3-1.xx&_nc_gid=APuX9q6HxL9hRDvgXeTLeq1&oh=00_AYCKzcjPddgLiOVjPVSHVOZx-KybbOgWns-yA7QuDL2yOQ&oe=66D7DC8D" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/124719069_3043499642416786_5648882886950543453_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=VNCph-_0WREQ7kNvgEil3nc&_nc_ht=scontent-sjc3-1.xx&oh=00_AYAbgkKXurOcFuenf9djjLh2tDp0e2bc_FglMv0RtrUPYQ&oe=66FD5D72" // Agent image URL
 };
 
 export default clientData;
